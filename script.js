ff7ace20a61dc6d9067a837e4138c2740635b3bb
yungdenvv <154081027+yungdenvv@users.noreply.github.com>
2024-01-17 02:18:36 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function openModal(src) {
+    var modal = document.getElementById("myModal");
+    var modalImg = document.getElementById("img01");
+
+    modal.style.display = "block";
+    modalImg.src = src;
+}
+
+
+function closeModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+}
